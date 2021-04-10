f8bfa57294b39ef446c69ce0ecfd2167bee02a89
mboss0@163.com <mboss0@163.com>
2021-04-10 18:36:16 +0800

    去掉 alias 插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
@@ -18,9 +17,6 @@ const config = {
     format: 'umd'
   },
   plugins: [
-    alias({
-      resolve: ['.js']
-    }),
     nodePolyfills(),
     resolve({
       preferBuiltins: false
